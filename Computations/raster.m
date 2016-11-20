3cded15518a7a3e01970281608b57cb0b0effaad
icydoge <icydoge@gmail.com>
2016-11-20 03:50:19 +0000

    Much faster surface fitting (from Will)
    Using matlab linear equation solving instead of the heavy fit function, making it now feasible to fit the entire 37000*32000 dataset! However, if we also do partial differentiations to find aspect, it is 10 times slower. So probably that job and slope analysis would be better for gdaldem. Through slope and aspect data we may be able to come back to MATLAB and run convexity analysis, etc.

--- Computations/raster.m
+++ Computations/raster.m
@@ -1,5 +1,6 @@
-SOURCE_RASTER = '~/Documents/NN2627.tif';
-TARGET_RASTER = '~/Documents/NN2627Fitted.tif';
+SOURCE_RASTER = '/mnt/Shared/OS5/Full/NN2627.tif';
+TARGET_RASTER = '/mnt/Shared/OS5/Full/NN2627Fitted.tif';
+ASPECT_RASTER = '/mnt/Shared/OS5/Full/NN2627Aspects.tif';
 
 % Disable warnings.
 parpool;
@@ -12,39 +13,77 @@ y_list = 1:size(rst,2);
 x_max = size(x_list,2) - 1;
 y_max = size(y_list,2) - 1;
 output = rst;
+aspects = zeros(size(rst));
 
 % Start timer.
 disp('Surface fittings started...');
 tic;
 
+% Use: z = ax^2 + by^2 + cxy + dx + ey + f
+% 
+% (-1,1)  (0,1)  (1,1)
+% (-1,0)  (0,0)  (1,0)
+% (-1,-1) (0,-1) (1,-1)
+%
+coords = [-1 1; 0 1; 1 1; -1 0; 0 0; 1 0; -1 -1; 0 -1; 1 -1];
+A(:,1) = coords(:,1).^2;
+A(:,2) = coords(:,2).^2;
+A(:,3) = coords(:,1).*coords(:,2);
+A(:,4) = coords(:,1);
+A(:,5) = coords(:,2);
+A(:,6) = 1;
+
 % Find neighbours of each point that is not on an edge, and choose the
 % points that have all valid neighbours (non-zero), fit a surface for the
 % neighbourhood and move the point onto the surface.
+parfor_progress(x_max);
 parfor x = 2:x_max
+    ux = sym('ux');
+    uy = sym('uy');
     for y = 2:y_max
         neighbours = pickneighbours(rst, x, y);
         if (any(isnan(neighbours(:))) == 0) && (any(neighbours(:,3)) == 1) % Check that boundaries are valid and no data is 0 (no-data in raster).
-            surface_fitted = fit([neighbours(:,1), neighbours(:,2)], neighbours(:,3), 'poly22');
+            z = neighbours(:,3);
+            C = A\z;
+            output(x,y) = C(6);
+            
             % Differentiate to compute gradients and therefore surface
             % normal
             % From normal, infer aspect
             % Aspect = atan2(ny,nx);
             % Slope angle will be to do with nz, theta = acos(nz);
             % ... curvature, convexity...
-            output(x,y) = surface_fitted(x,y);
+            % The following (aspect calculation makes this ten times
+            % slower when ran:
+            %{
+            equation = C(1) * ux^2 + C(2) * uy^2 + C(3) * ux * uy + C(4) * ux + C(5) * uy + C(6);
+            dx = diff(equation, ux);
+            dy = diff(equation, uy);
+            nx = -1 / single(subs(dx, [ux, uy], [0 0]));
+            ny = -1 / single(subs(dy, [ux, uy], [0 0]));
+            aspects(x, y) = rad2deg(atan2(ny, nx)); 
+            %}
+            
+            
         end
+        
     end
+    parfor_progress;
 end
+
+parfor_progress(0);
 
 % Stop timer and report performance.
 seconds = toc;
 fprintf('Surface fitting completed in %f seconds.\r', seconds);
-disp('Writing output to the target raster...');
+disp('Writing output to the target rasters...');
 
-% Write output raster.
-CoordRefSysCode = 27700;
+% Write output rasters.
+CoordRefSysCode = 27700; % British National Grid.
 geotiffwrite(TARGET_RASTER, output, raster_info, 'CoordRefSysCode', CoordRefSysCode);
 disp('Output written to the target raster, all done.');
+%geotiffwrite(ASPECT_RASTER, aspects, raster_info, 'CoordRefSysCode', CoordRefSysCode);
+%disp('Output written to the aspect raster, all done.');
 
-% Re-enable warnings.
-pctRunOnAll warning on;
+% Close off.
+delete(gcp('nocreate'));
