307a08417802564495419101d2bafdf673892cde
icydoge <icydoge@gmail.com>
2017-02-01 03:38:41 +0000

    Modify RASTER_MAX for current risk raster.

--- GeoData/rasters.py
+++ GeoData/rasters.py
@@ -3,4 +3,4 @@ ASPECT_RASTER = "/mnt/Shared/OS5/Full/WGSAspects.tif"
 CONTOUR_RASTER = "/mnt/Shared/OS5/Full/WGS_Map.tif"
 RISK_RASTER = "/mnt/Shared/OS5/Full/WGSStaticRisk.tif"
 RISK_RASTER_MIN = 0
-RISK_RASTER_MAX = 1
+RISK_RASTER_MAX = 0.0030
