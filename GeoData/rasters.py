6be255a38d4fa50cca43d36b88f1e9a7d8eaf99b
icydoge <icydoge@gmail.com>
2017-02-07 18:59:25 +0000

    Best visualisation at the moment.

--- GeoData/rasters.py
+++ GeoData/rasters.py
@@ -3,4 +3,4 @@ ASPECT_RASTER = "/mnt/Shared/OS5/Full/WGSAspects.tif"
 CONTOUR_RASTER = "/mnt/Shared/OS5/Full/WGS_Map.tif"
 RISK_RASTER = "/mnt/Shared/OS5/Full/WGSStaticRisk.tif"
 RISK_RASTER_MIN = 0
-RISK_RASTER_MAX = 0.15 # 99.6 percentile for the current raster.
+RISK_RASTER_MAX = 0.0913755 # 99 percentile for the current raster.
