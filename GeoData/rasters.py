d10be2efffa3f63ac631b3f4a28a1c91d8700281
icydoge <icydoge@gmail.com>
2017-02-07 18:45:01 +0000

    Better percentile to cover ridges.

--- GeoData/rasters.py
+++ GeoData/rasters.py
@@ -1,6 +1,6 @@
 HEIGHT_RASTER = "/mnt/Shared/OS5/Full/WGS.tif"
 ASPECT_RASTER = "/mnt/Shared/OS5/Full/WGSAspects.tif"
 CONTOUR_RASTER = "/mnt/Shared/OS5/Full/WGS_Map.tif"
-RISK_RASTER = "/mnt/Shared/OS5/Full/WGSRiskX.tif"
+RISK_RASTER = "/mnt/Shared/OS5/Full/WGSStaticRisk.tif"
 RISK_RASTER_MIN = 0
-RISK_RASTER_MAX = 0.01
+RISK_RASTER_MAX = 0.15 # 99.6 percentile for the current raster.
