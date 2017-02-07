f58aefc1f69fc618bc2aa3332b06732bf4467458
icydoge <icydoge@gmail.com>
2017-02-07 18:04:57 +0000

    New risk raster upper.

--- GeoData/rasters.py
+++ GeoData/rasters.py
@@ -3,4 +3,4 @@ ASPECT_RASTER = "/mnt/Shared/OS5/Full/WGSAspects.tif"
 CONTOUR_RASTER = "/mnt/Shared/OS5/Full/WGS_Map.tif"
 RISK_RASTER = "/mnt/Shared/OS5/Full/WGSRiskX.tif"
 RISK_RASTER_MIN = 0
-RISK_RASTER_MAX = 0.0030
+RISK_RASTER_MAX = 0.01
